7047ac103e9490b06128f3194265b5fc1972ad5f
branco0verflow <brann455@gmail.com>
2025-07-29 21:32:09 -0300

    Se agreó permisos

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,4 +1,4 @@
 
 
-export const API_BASE_URL = "http://localhost:8081/sgc/api/v1/";
-//export const API_BASE_URL = "https://tesisbackendfull.onrender.com/sgc/api/v1/";
+//export const API_BASE_URL = "http://localhost:8081/sgc/api/v1/";
+export const API_BASE_URL = "https://tesisbackendfull.onrender.com/sgc/api/v1/";
